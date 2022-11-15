d63b307e0f65bc60471a66b042313c74b992732d
Abel Guillen <abel.guillenfuerte@thoughtworks.com>
2022-11-15 13:50:38 +0100

    Create subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,23 @@ resource "aws_vpc" "aa_vpc" {
     name="aa_vpc"
   }
 }
+
+resource "aws_subnet" "aa_west_1a" {
+  vpc_id = aws_vpc.aa_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    name="aa_west_1a"
+  }
+}
+
+resource "aws_subnet" "aa_west_1b" {
+  vpc_id = aws_vpc.aa_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    name="aa_west_1b"
+  }
+}
