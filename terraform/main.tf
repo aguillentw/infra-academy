161936be7073186f93374cb67e34191a10acdc54
Abel Guillen <abel.guillenfuerte@thoughtworks.com>
2022-11-16 22:47:11 +0100

    Crate NLB

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "aws_internet_gateway" "aa_igw" {
   vpc_id = aws_vpc.aa_vpc.id
 
   tags = {
-    Name = "ag_vpc-igw"
+    Name = "aa_vpc-igw"
   }
 }
 
@@ -81,7 +81,7 @@ resource "aws_launch_configuration" "aa_launch_config" {
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   key_name                    = aws_key_pair.aa_another_key_pair.key_name
-  security_groups = [aws_security_group.aa_security.id]
+  security_groups             = [aws_security_group.aa_security.id]
 
   user_data = <<-EOF
 #!/bin/bash
@@ -97,10 +97,36 @@ resource "aws_autoscaling_group" "aa_scaling_group" {
   min_size             = 2
   launch_configuration = aws_launch_configuration.aa_launch_config.id
   vpc_zone_identifier  = [aws_subnet.aa_west_1a.id, aws_subnet.aa_west_1b.id]
+  target_group_arns = [aws_lb_target_group.aa_pool.arn]
 
   tag {
     key                 = "Name"
     propagate_at_launch = true
     value               = "aa_instance"
   }
+}
+
+resource "aws_lb" "aa_load_balancer" {
+  name                             = "aa-nlb"
+  internal                         = false
+  load_balancer_type               = "network"
+  enable_cross_zone_load_balancing = true
+  subnets                          = [aws_subnet.aa_west_1a.id, aws_subnet.aa_west_1b.id]
+}
+
+resource "aws_lb_listener" "aa_frontend" {
+  load_balancer_arn = aws_lb.aa_load_balancer.arn
+  port              = 80
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.aa_pool.arn
+  }
+}
+
+resource "aws_lb_target_group" "aa_pool" {
+  name     = "web-services"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.aa_vpc.id
 }
